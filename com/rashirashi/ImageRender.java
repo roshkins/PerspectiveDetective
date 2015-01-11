4eeb491622f1bb46f5d35e0703af5ed79edb9686
Rashi Abramson <roshkins@gmail.com>
2015-01-10 22:33:39 -0800

    Got images
    Here

---
+++ com/rashirashi/ImageRender.java
@@ -0,0 +1,150 @@
+package com.rashirashi;
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Canvas;
+import java.awt.Toolkit;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Image;
+import com.leapmotion.leap.ImageList;
+
+import javax.swing.JFrame;
+
+public class ImageRender extends Canvas implements Runnable
+{
+
+	private static final long	serialVersionUID	= 1L;
+
+	public static final int		WIDTH				= 1280, HEIGHT = 720;
+
+	public int					low, high;
+
+	private boolean				running				= true;
+	private Thread				thread;
+
+	static Toolkit				toolkit				= Toolkit.getDefaultToolkit();
+	private BufferedImage		image;
+	public int[]				pixels;
+	int							backColor			= 0x000000;
+	private boolean				scale				= true;
+
+	public static void main(String[] args)
+	{
+		ImageRender display = new ImageRender();
+		JFrame frame = new JFrame();
+		frame.add(display);
+		frame.setTitle("Perspective Detective");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension d = toolkit.getScreenSize();
+
+		frame.setLocation((d.width - WIDTH) / 2, (d.height - HEIGHT) / 2);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.pack();
+		display.start();
+		
+		//ImageList images = frame.images();
+		//camera.updatePixels();
+		//image(camera, 640 * image.id, 0)
+	}
+
+	public ImageRender()
+	{
+		Dimension size = new Dimension(WIDTH, HEIGHT);
+		setPreferredSize(size);
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+		
+		//Leap Image Constructor
+	
+	}
+
+	private void start()
+	{
+		run();
+	}
+
+	public void run()
+	{
+		Controller controller = new Controller();
+		controller.setPolicy(Controller.PolicyFlag.POLICY_IMAGES);
+		
+		while (running)
+		{
+			draw();
+			
+			 Frame frame = controller.frame();
+			 if(frame.isValid()){
+			   ImageList images = frame.images();
+			   for(Image image : images)
+			   {
+			     //Processing PImage class
+			    // BufferedImage camera = [image.id()];
+			   //  camera = createImage(image.width(), image.height(), RGB);
+			  //////   camera.loadPixels();
+			     
+			     //Get byte array containing the image data from Image object
+			     byte[] imageData = image.data();
+
+			     //Copy image data into display object, in this case PImage defined in Processing
+			     for(int i = 0; i < image.width() * image.height(); i++){
+			      int r = (imageData[i] & 0xFF) << 16; //convert to unsigned and shift into place
+			       int g = (imageData[i] & 0xFF) << 8;
+			      int  b = imageData[i] & 0xFF;
+			       pixels[i] =  r | g | b; 
+			     }
+			     
+			     //Show the image
+			//     camera.updatePixels();
+			//     image(camera, 640 * image.id(), 0);  
+			   }
+			 }
+			
+//			for (int x = 0; x < WIDTH * HEIGHT; x++)
+//			{
+//				pixels[x] = backColor;
+//			}
+			
+			
+		}	
+	}
+
+	public void draw()
+	{
+		BufferStrategy bs = this.getBufferStrategy();
+
+		if (bs == null)
+		{
+			createBufferStrategy(3);
+			return;
+		}
+
+		// 0x00B803
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH, HEIGHT, null);
+
+		g.dispose();
+		bs.show();
+	}
+
+	public void fillArea(int x1, int x2, int y1, int y2, int color)
+	{
+		for (int x = x1; x < x2; x++)
+		{
+			for (int y = y1; y < y2; y++)
+			{
+				pixels[y * WIDTH + x] = color;
+			}
+		}
+	}
+
+}
--- com/rashirashi/InitBit.java
+++ com/rashirashi/InitBit.java
@@ -1,10 +1,46 @@
 package com.rashirashi;
 
+import java.awt.image.BufferedImage;
+
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Image;
+import com.leapmotion.leap.ImageList;
+
 public class InitBit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	Controller controller = new Controller();
+	controller.setPolicy(Controller.PolicyFlag.POLICY_IMAGES);
+	
+	
+	 Frame frame = controller.frame();
+	 if(frame.isValid()){
+	   ImageList images = frame.images();
+	   for(Image image : images)
+	   {
+	     //Processing PImage class
+	     BufferedImage camera = [image.id()];
+	     camera = createImage(image.width(), image.height(), RGB);
+	     camera.loadPixels();
+	     
+	     //Get byte array containing the image data from Image object
+	     byte[] imageData = image.data();
 
+	     //Copy image data into display object, in this case PImage defined in Processing
+	     for(int i = 0; i < image.width() * image.height(); i++){
+	       r = (imageData[i] & 0xFF) << 16; //convert to unsigned and shift into place
+	       g = (imageData[i] & 0xFF) << 8;
+	       b = imageData[i] & 0xFF;
+	       camera.pixels[i] =  r | g | b; 
+	     }
+	     
+	     //Show the image
+	     camera.updatePixels();
+	     image(camera, 640 * image.id(), 0);  
+	   }
+	} 
 	}
 
 }
